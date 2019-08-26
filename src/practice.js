4f9333505e2eadf8500a4ecc1f844d03949cde1c
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-08-26 09:26:40 -0400

    complete checkpoint example

--- src/practice.js
+++ src/practice.js
@@ -8,11 +8,72 @@ const knexInstance = knex({
 
 console.log('connection successful');
 
-const qry = knexInstance
-  .select('product_id', 'name', 'price', 'category')
-  .from('amazong_products')
-  .where({ name: 'Point of view gun' })
-  .first()
-  .toQuery();
+// const qry = knexInstance
+//   .select('product_id', 'name', 'price', 'category')
+//   .from('amazong_products')
+//   .where({ name: 'Point of view gun' })
+//   .first()
+//   .toQuery();
 
-  console.log(qry);
+// console.log(qry);
+
+function searchByProductName(searchTerm) {
+    knexInstance
+      .select('product_id', 'name', 'price', 'category')
+      .from('amazong_products')
+      .where('name', 'ILIKE', `%${searchTerm}%`)
+      .then(result => {
+          console.log(result)
+      })
+}
+
+searchByProductName('holo');
+
+function paginateProducts(page) {
+    const productsPerPage = 10;
+    const offset = productsPerPage * (page - 1 )
+    knexInstance
+        .select('product_id', 'name', 'price', 'category')
+        .from('amazong_products')
+        .limit(productsPerPage)
+        .offset(offset)
+        .then(result => {
+            console.log(result)
+        })
+}
+
+paginateProducts(2);
+
+function getProductsWithImages() {
+    knexInstance
+        .select('product_id', 'name', 'price', 'category')
+        .from('amazong_products')
+        .whereNotNull('image')
+        .then(result => {
+            console.log(result)
+        })
+}
+
+getProductsWithImages();
+
+function mostPopularVideosForDays(days) {
+    knexInstance
+        .select('video_name', 'region')
+        .count('date_viewed AS views')
+        .where(
+            'date_viewed',
+            '>',
+            knexInstance.raw(`now() - '?? days'::INTERVAL`, days)
+            )
+        .from('whopipe_video_views')
+        .groupBy('video_name', 'region')
+        .orderBy([
+        { column: 'region', order: 'ASC' },
+        { column: 'views', order: 'DESC' },
+        ])
+        .then(result => {
+            console.log(result)
+        })
+}
+
+mostPopularVideosForDays(30);
