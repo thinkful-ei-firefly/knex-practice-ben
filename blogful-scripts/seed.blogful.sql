d2d6c39e9c17ba53d5b713db21a821b1d3406447
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-08-26 14:16:29 -0400

    up to adding articles

--- blogful-scripts/seed.blogful.sql
+++ blogful-scripts/seed.blogful.sql
@@ -1,27 +1,104 @@
 begin;
-
-insert into blogful_articles (title, date_published, content)
+insert into
+    blogful_articles (title, date_published, content)
 values
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
-('A cool blog', now() - '2 days'::interval, 'the most awesome content ever');
-
-commit;
-
+    (
+        'A cool blog',
+        now() - '1 days' :: interval,
+        'the most cool content ever'
+    ),
+    (
+        'A dumb blog',
+        now() - '3 days' :: interval,
+        'the most dumb content ever'
+    ),
+    (
+        'An awesome blog',
+        now() - '6 days' :: interval,
+        'the most awesome content ever'
+    ),
+    (
+        'A foo blog',
+        now() - '5 days' :: interval,
+        'the most foo content ever'
+    ),
+    (
+        'A bar blog',
+        now() - '7 days' :: interval,
+        'the most bar content ever'
+    ),
+    (
+        'A etc blog',
+        now() - '10 days' :: interval,
+        'the most et content ever'
+    ),
+    (
+        'A cool blog',
+        now() - '1 days' :: interval,
+        'the most cool content ever'
+    ),
+    (
+        'A dumb blog',
+        now() - '3 days' :: interval,
+        'the most dumb content ever'
+    ),
+    (
+        'An awesome blog',
+        now() - '6 days' :: interval,
+        'the most awesome content ever'
+    ),
+    (
+        'A foo blog',
+        now() - '5 days' :: interval,
+        'the most foo content ever'
+    ),
+    (
+        'A bar blog',
+        now() - '7 days' :: interval,
+        'the most bar content ever'
+    ),
+    (
+        'A etc blog',
+        now() - '10 days' :: interval,
+        'the most et content ever'
+    ),
+    (
+        'A cool blog',
+        now() - '1 days' :: interval,
+        'the most cool content ever'
+    ),
+    (
+        'A dumb blog',
+        now() - '3 days' :: interval,
+        'the most dumb content ever'
+    ),
+    (
+        'An awesome blog',
+        now() - '6 days' :: interval,
+        'the most awesome content ever'
+    ),
+    (
+        'A foo blog',
+        now() - '5 days' :: interval,
+        'the most foo content ever'
+    ),
+    (
+        'A bar blog',
+        now() - '7 days' :: interval,
+        'the most bar content ever'
+    ),
+    (
+        'A etc blog',
+        now() - '10 days' :: interval,
+        'the most et content ever'
+    ),
+    (
+        'A cool blog',
+        now() - '1 days' :: interval,
+        'the most cool content ever'
+    ),
+    (
+        'A etc blog',
+        now() - '10 days' :: interval,
+        'the most et content ever'
+    );commit;
