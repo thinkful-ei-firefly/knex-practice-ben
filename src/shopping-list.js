1187460835918a894796dd27371387032c2556f8
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-08-26 15:57:13 -0400

    update shopping-list.js

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -7,4 +7,24 @@ const knexInstance = knex({
   connection: process.env.DB_URL
 });
 
-console.log(ShoppingListService.getAllItem());
+ShoppingListService.getAllItems(knexInstance)
+  .then(items => console.log(items))
+  .then(() =>
+    ShoppingListService.insertItem(knexInstance, {
+      name: 'New item',
+      price: '7.82',
+      category: 'Breakfast',
+      checked: true,
+      date_added: new Date('2029-01-22T16:28:32.615Z')
+    })
+  )
+  .then(newItem => {
+    console.log(newItem);
+    return ShoppingListService.updateItem(knexInstance, newItem.id, {
+      name: 'Updated name'
+    }).then(() => ShoppingListService.getById(knexInstance, newItem.id));
+  })
+  .then(item => {
+    console.log(item);
+    return ShoppingListService.deleteItem(knexInstance, item.id);
+  });
