c857f6d815fb5c04cc467893f71aea5962aa5dfb
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-08-26 11:08:51 -0400

    begin blogful checkpoint

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,27 @@
+begin;
+
+insert into blogful_articles (title, date_published, content)
+values
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever'),
+('A cool blog', now() - '2 days'::interval, 'the most awesome content ever');
+
+commit;
+
