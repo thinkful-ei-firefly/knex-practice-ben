bc0131e818232485f7f4b0525a66ca6a1a8d024e
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-08-25 23:25:30 -0400

    up to searching amazong

--- src/practice.js
+++ src/practice.js
@@ -1,1 +1,18 @@
-console.log('hello, practice')
+require('dotenv').config()
+const knex = require('knex')
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL
+})
+
+console.log('connection successful');
+
+const qry = knexInstance
+  .select('product_id', 'name', 'price', 'category')
+  .from('amazong_products')
+  .where({ name: 'Point of view gun' })
+  .first()
+  .toQuery();
+
+  console.log(qry);
