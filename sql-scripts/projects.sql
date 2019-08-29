0cb2380d33d91a8ce4a36839e520168b58a82733
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-08-29 11:14:57 -0400

    format sql

--- sql-scripts/projects.sql
+++ sql-scripts/projects.sql
@@ -1,101 +1,62 @@
--- 1. How many people work in the sales department?
+-- 1. How many people work in the sales department? 
 
-select
-  count(e.emp_name),
-  d.dept_name
-from
-  employee e
-inner join
-  department d
-on
-  e.department = d.id
-where 
-  d.dept_name = 'Sales'
-group by
-  d.dept_name;
+SELECT Count(e.emp_name), 
+       d.dept_name 
+FROM   employee e 
+       INNER JOIN department d 
+               ON e.department = d.id 
+WHERE  d.dept_name = 'Sales' 
+GROUP  BY d.dept_name; 
 
--- 2. List the names of all employees assigned to the 'Plan Christmas party' project.
 
-select
-  e.emp_name,
-  p.project_name
-from
-  employee e 
-join 
-  employee_project ep
-on 
-  e.id = ep.emp_id
-join
-  project p
-on 
-  ep.project_id = p.id
-where 
-  p.project_name = 'Plan christmas party';
+-- 2. List the names of all employees assigned to the 'Plan Christmas party' project. 
+
+SELECT e.emp_name, 
+       p.project_name 
+FROM   employee e 
+       JOIN employee_project ep 
+         ON e.id = ep.emp_id 
+       JOIN project p 
+         ON ep.project_id = p.id 
+WHERE  p.project_name = 'Plan christmas party'; 
+
 
 -- 3. List the names of employees from the Warehouse department that are assigned to the 'Watch paint dry' project.
 
-select
-  e.emp_name
-from
-  employee e
-join
-  department d
-on
-  e.department = d.id
-join
-  employee_project ep
-on
-  e.id = ep.emp_id
-join
-  project p
-on
-  ep.project_id = p.id
-where
-  d.dept_name = 'Warehouse'
-and
-  p.project_name = 'Watch paint dry';
-  
-
--- 4. Which projects are the Sales department employees assigned to?
-
-select
-  e.emp_name,
-  p.project_name
-from
-  employee e
-join
-  department d
-on
-  e.department = d.id
-join
-  employee_project ep
-on
-  e.id = ep.emp_id
-join
-  project p
-on
-  ep.project_id = p.id
-where
-  d.dept_name = 'Sales';
+SELECT e.emp_name 
+FROM   employee e 
+       JOIN department d 
+         ON e.department = d.id 
+       JOIN employee_project ep 
+         ON e.id = ep.emp_id 
+       JOIN project p 
+         ON ep.project_id = p.id 
+WHERE  d.dept_name = 'Warehouse' 
+       AND p.project_name = 'Watch paint dry'; 
 
 
--- 5. List only the managers that are assigned to the 'Watch paint dry' project.
+-- 4. Which projects are the Sales department employees assigned to? 
 
-select
-  e.emp_name as managers
-from
-  department d
-join
-  employee e
-on
-  d.manager = e.id
-join
-  employee_project ep
-on
-  e.id = ep.emp_id
-join
-  project p
-on
-  ep.project_id = p.id
-where
-  p.project_name = 'Watch paint dry';
+SELECT e.emp_name, 
+       p.project_name 
+FROM   employee e 
+       JOIN department d 
+         ON e.department = d.id 
+       JOIN employee_project ep 
+         ON e.id = ep.emp_id 
+       JOIN project p 
+         ON ep.project_id = p.id 
+WHERE  d.dept_name = 'Sales'; 
+
+
+-- 5. List only the managers that are assigned to the 'Watch paint dry' project. 
+
+SELECT e.emp_name AS managers 
+FROM   department d 
+       JOIN employee e 
+         ON d.manager = e.id 
+       JOIN employee_project ep 
+         ON e.id = ep.emp_id 
+       JOIN project p 
+         ON ep.project_id = p.id 
+WHERE  p.project_name = 'Watch paint dry'; 
